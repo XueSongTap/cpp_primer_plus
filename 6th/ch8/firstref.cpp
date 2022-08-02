74143ca42aa4b964914221ef869bec08a916ecfe
XueSongTap <tapkindle@outlook.com>
2022-08-02 19:44:40 +0800

    init

---
+++ 6th/ch8/firstref.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main(){
+    using namespace std;
+
+    int rats = 101;
+
+    int & rodents = rats;
+
+
+    cout << "rats = " << rats;
+
+    cout << ", rodents = " << rodents << endl;
+
+    rodents ++;
+    cout << "rats =" << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    cout << "rats.adrress = " << &rats;
+
+    cout << ", rodents.address = " << &rodents << endl;
+    
+    return 0;
+}
